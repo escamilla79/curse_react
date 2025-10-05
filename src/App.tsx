707802df1b89f28807648f25750d6021ec56d000
jiescamilla <jiescamilla@izzi.mx>
2025-10-04 23:04:10 -0600

    Button: se cambia disabled el button con props

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import PrimerComponente from "./components/PrimerComponente";
 import SegundoComponente from "./components/SegundoComponente";
 import Card from "./components/Card";
 import ListItem from "./components/ListItem";
+import Button from "./components/Button";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
   const list = ["Goku", "tanjiro", "Eren"];
   const handleSelect = (element: string) => {
     console.log("Imprimiendo ", element);
@@ -18,6 +22,9 @@ function App() {
       <PrimerComponente></PrimerComponente>
       <SegundoComponente></SegundoComponente>
       <ListItem data={list} onSelect={handleSelect}></ListItem>
+      <Button isLoading={isLoading} onClick={handleClick}>
+        Hola mundo
+      </Button>
 
       <Card></Card>
     </>
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+
+type Props = {
+  isLoading: boolean;
+  children: ReactNode;
+  onClick: () => void;
+};
+
+function Button({ isLoading, children, onClick }: Props) {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        disabled={isLoading}
+        type="button"
+        className={`btn btn-${isLoading ? "primary" : "secondary"}}`}
+      >
+        {isLoading ? "Cargando ..." : children}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
