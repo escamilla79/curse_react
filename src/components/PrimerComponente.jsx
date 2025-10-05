847519c5c9054bac28e135f401c881aeee17bd3f
jiescamilla <jiescamilla@izzi.mx>
2025-10-04 19:24:34 -0600

    Componentes

---
+++ src/components/PrimerComponente.jsx
@@ -0,0 +1,5 @@
+const PrimerComponente = () => {
+  return <div>PrimeComponente</div>;
+};
+
+export default PrimerComponente;
---
+++ src/components/SegundoComponente.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SegundoComponente = () => {
+  return <div>SegundoComponente</div>;
+};
+
+export default SegundoComponente;
